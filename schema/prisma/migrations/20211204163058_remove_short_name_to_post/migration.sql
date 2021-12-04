a48455a5b6c9a70a1619000898ba5dd8530e610c
leovs09 <leovs010@gmail.com>
2021-12-04 17:31:16 +0100

    remove short name from post

---
+++ schema/prisma/migrations/20211204163058_remove_short_name_to_post/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `short_name` on the `Post` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Post" DROP COLUMN "short_name";
